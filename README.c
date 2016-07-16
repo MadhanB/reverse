b999e554e60d580e76cab708e6eb8d2e86937f7b
MadhanB <smartmadhan07@gmail.com>
2016-07-16 16:01:08 +0530

    finish

---
+++ README.c
@@ -0,0 +1,14 @@
+# reverse
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	int i;
+char text[50];
+clrscr();
+puts("enter the string");
+gets(text);
+puts("Reverse string":);
+for(i=50;i>=0;i--)
+printf("%c",text[i]);
+}
